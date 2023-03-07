8c271c73cf1d63b56aad71c1af44cd0e5e084255
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-03-07 17:39:50 +0100

    a

--- game.php
+++ game.php
@@ -9,6 +9,9 @@
 </head>
 <body>
     <h1 id="title2">Game is ongoing</h1>
+    <form method="post">
+        <input type="submit" name="win" value="Start">
+    </form>
     <div>
     <?php
     function generateQuestion(){
@@ -18,7 +21,7 @@
         foreach($files as $file){
             $songs[] = $file;
             $songNames[] = trim(str_replace('songs/','',$file));
-            echo $file." | ".trim(str_replace('songs/','',$file))."<br>";
+            //echo $file." | ".trim(str_replace('songs/','',$file))."<br>";
         }
         $song = $songs[rand(0,count($songs)-1)];
         $theSongName = trim(str_replace('songs/','',$song)); $theSongName = trim(str_replace('.mp3','',$song));
@@ -26,25 +29,27 @@
         <audio controls autoplay>
         <source src='$song' type='audio/mp3'>
         </audio><br>");
-        ?>
-        <?php
         $array = array();
-        $one = "<button id='win' type='button' onclick='win()' style='background-color:lightgreen;'>".trim(str_replace('.mp3','',trim(str_replace('songs/','',$song))))."</button>";
+        $one = "<input id='win' type='submit' name='win' style='background-color:lightgreen;' value='".trim(str_replace('.mp3','',trim(str_replace('songs/','',$song))))."'/>";
         array_push($array, $one);
         array_splice($songNames,array_search(trim(str_replace('songs/','',$song)),$songNames), array_search(trim(str_replace('songs/','',$song)),$songNames));
         shuffle($songNames);
         for($n = 0; $n < 3; $n++){
-            array_push($array, "<button type='button' onclick='lose()'>".trim(str_replace('.mp3','',$songNames[$n])))."</button>";
+            array_push($array, "<button type='button' name='lose' id='lose'>".trim(str_replace('.mp3','',$songNames[$n]))."</button>");
         }
         shuffle($array);
         return $array;
     }
     function printQuestions(){
+        echo '<form method="post" onsubmit="lose(); return false;">';
         foreach(generateQuestion() as $element){
             echo($element);
         }
+        echo '</form>';
     }
-    printQuestions();
+    if(isset($_POST['win'])){
+        printQuestions();
+    }
     ?>
     </div>
 </body>
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function win(){
     numberOfCorrect++;
     document.getElementById("title2").innerHTML = "You WIN: Total of " + numberOfCorrect + " times";
 }
-function lose(){
+document.getElementById("lose").addEventListener("click", function lose(){
     numberOfLosses++;
     document.getElementById("title2").innerHTML = "You LOST: Total of " + numberOfLosses + " times";
-}
+});
